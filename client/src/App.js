dec1a6c700cec98c304048cb705914b891dd734e
baydogan <aydoganbugra93@gmail.com>
2022-09-05 14:19:07 +0300

    routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { useRoutes } from "react-router-dom";
+import routes from "routes/index";
 import Navbar from "layouts/Navbar";
 
 function App() {
+  const showRoutes = useRoutes(routes);
+
   return (
     <ChakraProvider>
       <div className="App">
         <Navbar />
+        <>
+         {showRoutes}
+        </>
       </div>
     </ChakraProvider>
   );
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import { Heading } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <>
+      <Heading as="h1" size="4xl" noOfLines={1}>
+        Latest Products
+      </Heading>
+    </>
+  );
+};
+
+export default Home;
